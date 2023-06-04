495c9404b1e3afb93984ee0f4b06c77043b89823
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-06-03 23:22:36 -0400

    Had to change a few things but i got it working

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# SKE-Work-Day-Scheduler
+# Workday Scheduler 
+
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+
+## Description
+
+Given started code, the goal of this challenge was to create a simple calendar application that allows a user to save events for each hour of the day to make this workday scheduler operational.
+
+
+
+
+
+# Usage
+Clone repo and open the application in your live server. 
+
+
+# Credits
+
+* Shamary Edwards
+
+
+# License 
+  The MIT License
+   
+  (https://opensource.org/licenses/MIT)
+  
--- script.js
+++ script.js
@@ -1,67 +1,57 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
 
+var todayDate = moment().format('DD, MM, YYYY');
+$("#currentDay").html(todayDate);
 
-  // Displaying the date and time. Used military time because i thought it would be easier to work with.
-  var today = dayjs();
-  $('#currentDay').text(today.format('dddd, MMMM D YYYY'));
+$(document).ready(function () {
+   
+    $(".saveBtn").on("click", function () {
+       
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-  var time = dayjs();
-  $('#currentTime').text(time.format('HH:MM A'));
+      
+        localStorage.setItem(time, text);
+    })
+   
+    function timeChecker() {
+       
+        var timeCurrent = moment().hour();  // Gets current number of hours.
 
+        
+        $(".time-block").each(function () {   // Loops over time blocks
+            var blockTime = parseInt($(this).attr("id").split("hour-")[1]);
 
+            // Check the time and add the classes for background indicators
+            if (blockTime < timeCurrent) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeCurrent) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
 
+            }
+        })
+    }
 
+    // Get item from local storage if any
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-  //Not sure if this is too messy but it actually works so :-#.
-//Caparing the time on the schedule with day
-
-  var currentHour = dayjs().hour();
-var timeBlocks = document.querySelectorAll('.time-block');
-var scheduleTime;
-for(var x = 0; x < timeBlocks.length; x++) {
- 
-  if(compareTime = timeBlocks[x].id.length <= 7){
-     parseInt(timeBlocks[x].id.slice(0)) }
-  
-     else{
-     parseInt(timeBlocks[x].id.slice(-2));
-     }
-
-  if (currentHour > scheduleTime) {
-    timeBlocks[x].classList.add('past');
-  } else if (currentHour === scheduleTime) {
-    timeBlocks[x].classList.add('present');
-  } else {
-    timeBlocks[x].classList.add('future');
-  }
-};
-
-
-
-    
-
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-
-  
+    timeChecker();
+})
